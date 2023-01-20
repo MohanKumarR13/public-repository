a8ffbfae58325e68da8fc1aa43de64df1d72fe29
MohanKumarR13 <123152359+MohanKumarR13@users.noreply.github.com>
2023-01-20 22:54:33 +0530

    added
    added

--- Abstract.java
+++ Abstract.java
@@ -3,6 +3,8 @@ package Abstract;
 public abstract class Abstract {
 	public void education() {
 		System.out.println("Study");
+		System.out.println("BE");
+
 	}
 	public void work() {
 		System.out.println("Working");
